72d5a37c98ea3ab20fa51a9ac0f0e3d8f5469cc6
Anshika Tank <anshikatank2110@gmail.com>
2025-08-20 03:15:35 +0530

    Updated CORS for Frontend

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ const io = new Server(server, {
   },
 });
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // Health check endpoint
